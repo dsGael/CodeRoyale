0b78f87cdb60768acb04a47408aaee13d3264201
Jorge Luis <jorgedrluisj4@gmail.com>
2026-01-22 10:40:25 -0700

    Avances de la clase

--- app/src/main/java/com/example/coderoyale/MainActivity.kt
+++ app/src/main/java/com/example/coderoyale/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.coderoyale
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.coderoyale.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.BottomNV.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.Item_Inicio->{
+                    true
+                }
+                R.id.Item_Amigos->{
+                    true
+                }
+                R.id.Item_Leaderboard->{
+                    true
+                }
+                R.id. ->{
+                    true
+                }
+                R.id.Item_Cuenta->{
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
         }
     }
 }
