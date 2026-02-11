116e74091fd839286d61297cac0fd29681af0aa9
Jorge Luis <jorgedrluisj4@gmail.com>
2026-02-11 12:36:53 -0700

    avance

--- app/src/main/java/com/example/coderoyale/OpcionesLogin.kt
+++ app/src/main/java/com/example/coderoyale/OpcionesLogin.kt
@@ -8,17 +8,33 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.coderoyale.Opciones_Login.ActivityLoginEmail
 import com.example.coderoyale.databinding.ActivityLoginOptionsBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class OpcionesLogin : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginOptionsBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginOptionsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        comprobarSesion()
+
         binding.IngresarEmail.setOnClickListener {
             startActivity(Intent(this@OpcionesLogin, ActivityLoginEmail::class.java))
         }
     }
+
+    private fun comprobarSesion(){
+        if(firebaseAuth.currentUser != null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finishAffinity()
+        }
+    }
+
+
 }
