2f13160298ca9c94b32e3a784ebd799f868284f2
Jorge Luis <jorgedrluisj4@gmail.com>
2026-02-25 02:10:16 -0700

    avances de todo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebaseAuth)
     implementation(libs.firebaseDatabase)
+    implementation(libs.loginGoogle)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
